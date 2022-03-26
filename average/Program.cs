e8aca6467ca6b1486219d6123e43b5115ef7010e
Arvin-2005 <102425221+Arvin-2005@users.noreply.github.com>
2022-03-26 20:44:19 +0430

    Add files via upload

---
+++ average/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int s = 0;
+            int r;
+            for (int i = 1; i <= 5; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                s = s + a;
+            }
+            r = s / 5;
+
+            Console.Write(r);
+        }
+    }
+}
